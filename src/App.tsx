eaa84b48c95ef332a2182ae3d05dd232968b1d3e
Hoerin Doh <hellodhlyn@gmail.com>
2022-01-29 22:06:50 +0900

    Add prettier, react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
